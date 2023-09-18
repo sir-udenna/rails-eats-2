82d2c9adb2b72e0d66dd0e9afeabeeceebdec118
sir-udenna <udenna@DESKTOP-AINSI3S.localdomain>
2023-09-18 17:42:42 -0500

    implemented devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   resources :likes
   resources :follows
   resources :categories
--- db/schema.rb
+++ db/schema.rb
@@ -76,13 +76,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_165817) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.text "bio"
     t.text "favorite_food"
     t.text "recently_visited_places"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "follows", "businesses"
